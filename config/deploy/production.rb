22219b6eae92739c24f82fe7cb561a7ee62e58dd
sashmarins <sasha.marinskiiy@gmail.com>
2022-09-06 11:33:24 -0400

    change ubuntu to root

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "137.184.67.74", user: "ubuntu", roles: %w{app db web}
+server "137.184.67.74", user: "root", roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 app = ENV['APP']
@@ -27,9 +27,9 @@ set :ssh_options, { forward_agent: true }
 # property set. Specify the username and a domain or IP for the server.
 # Don't use `:all`, it's a meta role.
 
-role :app, %w{ubuntu@137.184.67.74}
-role :web, %w{ubuntu@137.184.67.74}
-role :db,  %w{ubuntu@137.184.67.74}
+role :app, %w{root@137.184.67.74}
+role :web, %w{root@137.184.67.74}
+role :db,  %w{root@137.184.67.74}
 
 
 
